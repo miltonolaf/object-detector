a2f083fa2469b7516bb9767332436badd9534a38
Milton Olaf Paredes <miltonolaf@outlook.com>
2018-12-06 21:23:50 -0600

    get config file

---
+++ tf_object_detector/config.py
@@ -0,0 +1,27 @@
+import json
+import click
+import shutil
+import os
+
+
+class Config(object):
+    """docstring for ."""
+
+    def __init__(self):
+        super(Config, self).__init__()
+        self.project_path = os.getcwd()
+        self.config_definition()
+        print(os.path.dirname(os.path.abspath(__file__)))
+
+    def get_config(self):
+        try:
+            with open(self.project_path + '/tod.config.json') as json_file:
+                json_file = json_file.read()
+                self.config_data = json.loads(json_file)
+        except Exception as e:
+            click.echo('We did not find your configuration file')
+
+    def config_definition(self):
+        self.get_config()
+        self.DOWNLOADED_MODELS_DIR = self.config_data['models_dir']
+        self.DEMO_MODELS = self.config_data['demo_models']
